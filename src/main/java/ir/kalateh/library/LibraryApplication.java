047c049772230a40e3ee24a19c96eb163da721d6
hossein <h.kalateh72@gmail.com>
2020-08-30 18:06:26 +0430

    first commit - start project

---
+++ src/main/java/ir/kalateh/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package ir.kalateh.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/ir/kalateh/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.kalateh.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
