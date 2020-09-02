4109216500a57589023278a20a093618e786e5a5
hossein <h.kalateh72@gmail.com>
2020-09-03 03:41:50 +0430

    add BookController

---
+++ src/main/java/ir/kalateh/library/controller/BookController.java
@@ -0,0 +1,26 @@
+package ir.kalateh.library.controller;
+
+import ir.kalateh.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    
+    private final BookRepository bookRepository;
+    
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books",bookRepository.findAll());
+        return "books";
+    }
+    
+    
+}
